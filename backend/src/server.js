3e2396d89f35811bb1227f51d96b939f0be3e928
YankoDelgado <129774332+YankoDelgado@users.noreply.github.com>
2025-06-30 16:58:19 -0400

    UPdate

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,6 +19,8 @@ app.use(
     cors({
         origin: process.env.VITE_API_URL || "http://localhost:5173",
         credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     }),
 )
 app.use(express.json())
