2a2fab9bc6d6cedf419b8956e48e2ac6400c499a
YankoDelgado <129774332+YankoDelgado@users.noreply.github.com>
2025-06-30 19:11:40 -0400

    cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT
 //Middleware
 app.use(
     cors({
-        origin: process.env.VITE_API_URL || "http://localhost:5173",
+        origin: [process.env.VITE_API_URL, "http://localhost:5173"],
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
